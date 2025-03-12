006142aa04c303b583723c38e77478aa9d51d799
D-Bibileishvili <d.bibileishvili19@gmail.com>
2025-03-12 17:44:46 +0400

    add validation

--- app.js
+++ app.js
@@ -1,35 +1,115 @@
 function formValidaton() {
-  const emailRegExp = /!@#$%/;
-
   const form = document.querySelector("#form");
   const emailInput = document.querySelector("#email");
   const nameInput = document.querySelector("#name");
   const passwordInput = document.querySelector("#password");
   const ageInput = document.querySelector("#age");
+  const confirm = document.querySelector("#ConfPassword");
+  const personal = document.querySelector("#personal-number");
+  const mobile = document.querySelector("#mobile-number");
 
   const showErrorMessage = (input, message) => {
+    input.closest(".form-group").classList.remove("success");
+    input.closest(".form-group").classList.add("error");
+    input.closest(".form-group").querySelector(".error-message").textContent =
+      message;
+  };
+  const showSuccessMessage = (input, message) => {
+    input.closest(".form-group").classList.remove("error");
+    input.closest(".form-group").classList.add("success");
     input.closest(".form-group").querySelector(".error-message").textContent =
       message;
   };
 
   const isNameValid = () => {
-    console.log(nameInput.value);
     const val = nameInput.value.trim();
     if (val === "") {
-      nameInput.closest(".form-group").classList.remove("success");
-      nameInput.closest(".form-group").classList.add("error");
       showErrorMessage(nameInput, "Name is required");
+      return false;
+    } else if (val.length < 3) {
+      showErrorMessage(nameInput, "Name is too short");
+      return false;
     } else {
-      nameInput.closest(".form-group").classList.remove("error");
-      nameInput.closest(".form-group").classList.add("success");
-
-      showErrorMessage(nameInput, "Name is valid");
+      showSuccessMessage(nameInput, "Name is valid");
+      return true;
     }
   };
 
+  const isEmailValid = () => {
+    const val = emailInput.value.trim();
+    const emailRegExp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (val === "") {
+      showErrorMessage(emailInput, "Email is required");
+      return false;
+    } else if (!emailRegExp.test(val)) {
+      showErrorMessage(emailInput, "Email is not correct format");
+      return false;
+    } else if (!/@gmail\.com$/.test(val)) {
+      showErrorMessage(emailInput, "Email must be gmail.com");
+      return false;
+    } else {
+      showSuccessMessage(emailInput, "Email is valid");
+      return true;
+    }
+  };
+
+  const isAgeValid = () => {
+    const val = ageInput.value.trim();
+    if (Number(val) < 0 || Number(val) > 10) {
+      showErrorMessage(ageInput, "Age must be between 0 and 10");
+      return false;
+    } else {
+      showSuccessMessage(ageInput, "Age is valid");
+      return true;
+    }
+  };
+
+  const isPasswordValid = () => {
+    const val = passwordInput.value.trim();
+    if (val.length <= 6) {
+      showErrorMessage(passwordInput, "Password must be min 6 characters");
+      return false;
+    } else {
+      showSuccessMessage(passwordInput, "Password is valid");
+      return true;
+    }
+  };
+
+  const isConfPassword = () => {
+    const val = confirm.value.trim();
+    if (val === "") {
+      showErrorMessage(passwordInput, "Confirm-Password is valid");
+      return false;
+    } else if (val !== passwordInput.value) {
+      showErrorMessage(
+        passwordInput,
+        "Confirm-Password must be same as Password "
+      );
+      return false;
+    } else {
+      showSuccessMessage(passwordInput, "Password is valid");
+      return true;
+    }
+  };
+
+  nameInput.addEventListener("input", isNameValid);
+  emailInput.addEventListener("input", isEmailValid);
+  ageInput.addEventListener("input", isAgeValid);
+  passwordInput.addEventListener("input", isPasswordValid);
+  confirm.addEventListener("input", isConfPassword);
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    isNameValid();
+    const nameValid = isNameValid();
+    const emailValid = isEmailValid();
+    const ageValid = isAgeValid();
+    const passwordValid = isPasswordValid();
+    const confirmpass = isConfPassword();
+
+    if (nameValid && emailValid && ageValid && passwordValid && confirmpass) {
+      console.log("form is valid");
+    }
   });
 }
 
